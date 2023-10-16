0528b87895592698e91aaeecd17f5a66b18cf49a
Your Name <sharunnd11@gmail.com>
2023-10-16 23:26:19 +0530

    basic backend structure

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const connectDB= async()=>{
+    try {
+        const connection =await mongoose.connect(process.env.MONGO_URL)
+        console.log("Connected to db");
+    } catch (error) {
+        console.log(error);
+        process.exit(1)
+    }
+}
+
+module.exports = {
+    connectDB
+}
