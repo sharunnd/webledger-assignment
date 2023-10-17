0ea454f7255efd095e6e6b79064d6d7925e9db9f
Your Name <sharunnd11@gmail.com>
2023-10-17 20:46:23 +0530

    login and registration completed

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { connectDB } = require("./db");
 require("dotenv").config()
-const cors = require("cors")
+const cors = require("cors");
+const { userRouter } = require("./routes/user.routes");
 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use("/users",userRouter)
 
- 
 connectDB().then(()=>{
     app.listen(process.env.PORT,()=>{
         try {
