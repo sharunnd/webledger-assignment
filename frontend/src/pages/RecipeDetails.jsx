95042b5c9f169893fe8adb542ddf8e752efb57f2
Your Name <sharunnd11@gmail.com>
2023-10-20 21:11:38 +0530

    recipe saving done

--- frontend/src/pages/RecipeDetails.jsx
+++ frontend/src/pages/RecipeDetails.jsx
@@ -1,23 +1,59 @@
-import { Box, Button, Flex, HStack, Image, Text } from "@chakra-ui/react";
+import { Button, Flex, HStack, Image, Text, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export default function RecipeDetails() {
   const id = useSelector((store) => store.recipeSearchReducer.id);
-
+  const token = Cookies.get("token")
+  const toast = useToast()
   const recipeDetails = useSelector(
     (store) => store.recipeSearchReducer.recipeDetails
   );
 
-  useEffect(() => {}, [recipeDetails]);
+  useEffect(() => {
+    
+    
+  }, [recipeDetails]);
 
-  console.log("erci....", recipeDetails);
   // Process instructions to remove <ol> and <li> tags
-  const processedInstructions = recipeDetails.instructions.replace(
-    /<ol>|<\/ol>|<li>|<\/li>/g,
-    ""
-  );
-  const handleSave = () => {};
+  const processedInstructions = recipeDetails.instructions?.replace(/<ol>|<\/ol>|<li>|<\/li>/g,"");
+  const handleSave = () => {
+    const recipeId = { recipeId:id };
+    console.log("recipeId",recipeId);
+    try {
+      axios
+       .post(`http://localhost:8080/recipes/save`,recipeId, {
+         headers: {
+           Authorization: `Bearer ${token}`,
+           "Content-Type": "application/json",
+         },
+       })
+       .then((res) => {
+         console.log(res);
+         toast({
+          position: 'top',
+          title: `${res.data.message}`,
+          status: 'success',
+          duration: 1500,
+          isClosable: true,
+        })
+       })
+       .catch((err) => {
+         console.log(err);
+         toast({
+          position: 'top',
+          title: `${err.response.data.error}`,
+          status: 'error',
+          duration: 1500,
+          isClosable: true,
+        })
+       });
+   } catch (error) {
+    console.log(error);
+   }
+  };
   return (
     <HStack m={"auto"}>
       <Flex
@@ -33,7 +69,9 @@ export default function RecipeDetails() {
         <Text fontWeight={600} fontSize={{ base: "15px", lg: "25px" }} mt={5}>
           {recipeDetails.title}
         </Text>
-        <Button onClick={handleSave} bg="#dbdbf5">Save Recipe</Button>
+        <Button onClick={handleSave} bg="#dbdbf5">
+          Save Recipe
+        </Button>
         <Text>Vegetarian: {recipeDetails.vegetarian ? "True" : "False"}</Text>
         <Text>DairyFree: {recipeDetails.dairyFree ? "True" : "False"}</Text>
         <Text>
